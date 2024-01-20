768da3de04a759e3a7527d1ef16944e16a47595d
VKWHM <111775089+VKWHM@users.noreply.github.com>
2024-01-20 13:13:46 +0300

    Update MainController.php
    Get date object from id query parameter and pass it to template

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CalendarDateRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
@@ -11,15 +12,20 @@ class MainController extends AbstractController
 {
     #[Route('/', name: 'app_main', methods: ['GET', 'HEAD'])]
     public function index(
+        CalendarDateRepository $calendarDateRepository,
         #[MapQueryParameter(
             name: "id",
             filter: \FILTER_VALIDATE_REGEXP,
             options: ['regexp' => '/^\d{1,4}$/']
-        )] ?int $id = null
+        )] ?int $id = null,
     ): Response {
+        if ($id) {
+            $date = $calendarDateRepository->find($id) or throw $this->createNotFoundException("Date not found");
+        }
+
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
-            'id' => $id
+            'date' => $date ?? null,
         ]);
     }
 }
